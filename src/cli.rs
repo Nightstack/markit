1654095ddf9e6a60fd94065036bd6e817cbc0878
Ryan Palmer <rpalmer9696@gmail.com>
2025-06-15 10:12:25 +0100

    Adds scaffolding for commands

---
+++ src/cli.rs
@@ -0,0 +1,17 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "markit")]
+#[command(about = "A CLI snippet runner/bookmarker", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Save { name: String },
+    Run { name: String },
+    List,
+    Show { name: String },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod cli;
+use clap::Parser;
+use cli::{Cli, Commands};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Save { name } => {
+            println!("Would save command '{}'", name);
+        }
+        Commands::Run { name } => {
+            println!("Would run command '{}'", name);
+        }
+        Commands::List => {
+            println!("Would list all bookmarks");
+        }
+        Commands::Show { name } => {
+            println!("Would show snippet '{}'", name);
+        }
+    }
 }
