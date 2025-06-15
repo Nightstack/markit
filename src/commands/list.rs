10f16daf8fe2745637848f1b340927f475b03ab5
Ryan Palmer <rpalmer9696@gmail.com>
2025-06-15 12:19:04 +0100

    Changes list output to a table

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,16 +1,39 @@
-use crate::storage;
+use crate::{models::SnippetStore, storage};
+use comfy_table::{Cell, Color, Row, Table, presets::UTF8_FULL};
 
 pub fn list_command() -> () {
     match storage::get_snippets() {
         Some(store) => {
-            println!("📚 Saved Snippets:\n");
-
-            for snippet in store.snippets {
-                println!("🔹 {}\n   {}", snippet.name, snippet.description);
-            }
+            let table = build_output_table(store);
+            println!("{table}");
         }
         None => {
             println!("📭 No snippets saved yet.");
         }
     }
+}
+
+fn build_output_table(store: SnippetStore) -> Table {
+    let mut table = Table::new();
+    table.load_preset(UTF8_FULL);
+    let header_color = Color::Rgb {
+        r: 100,
+        g: 255,
+        b: 255,
+    };
+
+    // Set colored header
+    table.set_header(vec![
+        Cell::new("Name").fg(header_color),
+        Cell::new("Description").fg(header_color),
+    ]);
+
+    for snippet in store.snippets {
+        table.add_row(Row::from(vec![
+            Cell::new(snippet.name).fg(Color::White),
+            Cell::new(snippet.description).fg(Color::White),
+        ]));
+    }
+
+    table
 }
